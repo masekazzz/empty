ef7c6303fc36277d8da970e1e9b8318e64188b44
Maselskas <masekas123@gmail.com>
2023-04-16 15:17:27 +0300

    initial commit

---
+++ src/main/java/ru/example/securitygovno/AController.java
@@ -0,0 +1,13 @@
+package ru.example.securitygovno;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AController {
+    @PostMapping(path = "/auth/login")
+    public ResponseEntity<String> login(){
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/ru/example/securitygovno/config/AConfiguration.java
@@ -0,0 +1,19 @@
+package ru.example.securitygovno.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class AConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .httpBasic().disable();
+        return http.build();
+    }
+}
